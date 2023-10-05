484bb57a585ae4c748b1bfe96b09e5bc09c628fb
Damian <ruiz.damian17@gmail.com>
2023-10-05 15:01:15 -0700

    Constants typings

--- src/app/constants.tsx
+++ src/app/constants.tsx
@@ -1,3 +1,3 @@
-export const DEFAULT_COLUMN_COUNT = 4
-export const DEFAULT_ROW_COUNT = 4
-export const DEFAULT_WIN_REQUIRED_COUNT = 4
+export const DEFAULT_COLUMN_COUNT: number = 4
+export const DEFAULT_ROW_COUNT: number = 4
+export const DEFAULT_WIN_REQUIRED_COUNT: number = 4
